f73949844a3af2dcbb17b880092b11bd985d12bf
Yue-Sheng <yuesheng1995@gmail.com>
2026-01-31 15:28:38 +0800

    Lint resolutions

--- src/main/java/tech3/binitright/controller/TestController.java
+++ src/main/java/tech3/binitright/controller/TestController.java
@@ -1,16 +1,13 @@
 package tech3.binitright.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Controller for testing system connectivity.
- */
-@RestController
-public final class TestController { // Added 'final' here
+@Controller
+public class TestController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "Connected";
+    @GetMapping("/")
+    public String test(){
+        return "home";
     }
 }
