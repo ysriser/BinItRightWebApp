9543f94ce2884c0576fc7f1c595a0103fce8ade5
Yue-Sheng <yuesheng1995@gmail.com>
2026-01-31 15:30:09 +0800

    Lint resolutions

--- src/main/java/tech3/binitright/controller/TestController.java
+++ src/main/java/tech3/binitright/controller/TestController.java
@@ -3,11 +3,18 @@ package tech3.binitright.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Controller for handling home page requests.
+ */
 @Controller
-public class TestController {
+public final class TestController { // Added 'final' here
 
+    /**
+     * Maps the root URL to the home view.
+     * @return the name of the home template
+     */
     @GetMapping("/")
-    public String test(){
+    public String test() {
         return "home";
     }
 }
