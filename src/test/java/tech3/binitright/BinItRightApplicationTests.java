033066728372476673910a78900410f370645097
Maha <rselvimaha@gmail.com>
2026-02-01 14:21:31 +0800

    Integrated bug fix

--- src/test/java/tech3/binitright/BinItRightApplicationTests.java
+++ src/test/java/tech3/binitright/BinItRightApplicationTests.java
@@ -2,12 +2,13 @@ package tech3.binitright;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // This tells Spring to use application-test.properties
 class BinItRightApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // This ensures your JPA entities and Repositories are mapped correctly
+    }
 }
