599dda4f39430c02143e3d86d0b21a161e05a055
Muthu-raj22 <e1539335@u.nus.edu>
2026-02-04 12:48:58 +0800

    Implemented Recycled history feature

---
+++ src/main/java/tech3/binitright/response/RecycleHistoryResponse.java
@@ -0,0 +1,38 @@
+package tech3.binitright.response;
+
+import java.time.LocalDateTime;
+
+public class RecycleHistoryResponse {
+
+    private String categoryName;
+    private String categoryIcon;
+    private LocalDateTime date;
+    private int quantity;
+
+    public RecycleHistoryResponse(String categoryName,
+                                  String categoryIcon,
+                                  LocalDateTime date,
+                                  Integer quantity
+    ) {
+        this.categoryName = categoryName;
+        this.categoryIcon = categoryIcon;
+        this.date = date;
+        this.quantity = quantity;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public String getCategoryIcon() {
+        return categoryIcon;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
