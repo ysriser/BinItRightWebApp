91695b577ea626d7df4a837dc2fcd45d562a886c
e1616033 <150251429+lachupriya0993@users.noreply.github.com>
2026-02-07 18:23:01 +0800

    Update JmxGenerator.java

--- src/test/java/tech3/binitright/performance/JmxGenerator.java
+++ src/test/java/tech3/binitright/performance/JmxGenerator.java
@@ -76,17 +76,11 @@ public class JmxGenerator {
                     httpSampler("API_GET_News_List",
                             baseUrl + "/api/news"), // news api
 
-                    httpSampler("API_GET_News_By_Id",
-                            baseUrl + "/api/news/1"), // news api with id =1
-
                     httpSampler("API_GET_Events_All",
                             baseUrl + "/api/events"), //event api 
                     
                     httpSampler("API_GET_RECYCLE_HISTORY",
                                 baseUrl + "/api/recycle-history"), // recycle history api
-                    
-                    httpSampler("API_GET_Events_Upcoming",
-                            baseUrl + "/api/events?filter=upcoming") // events api with filter 
                 )
                 
         ).saveAsJmx("tests/load_test.jmx");
