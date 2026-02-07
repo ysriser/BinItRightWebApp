4405d88060a069f7be587722e846e68caf84f2e2
e1616033 <150251429+lachupriya0993@users.noreply.github.com>
2026-02-07 11:52:44 +0800

    Update JmxGenerator.java with tets url

--- src/test/java/tech3/binitright/performance/JmxGenerator.java
+++ src/test/java/tech3/binitright/performance/JmxGenerator.java
@@ -6,11 +6,24 @@ import java.time.Duration;
 import org.apache.http.entity.ContentType;
 
 public class JmxGenerator {
-    public static void main(String[] args) throws IOException {
-        String host = System.getProperty("target_host", "localhost");
-        int port = Integer.getInteger("target_port", 8080);
-        String baseUrl = "http://" + host + ":" + port;
-         String defaultUser = System.getProperty("perf_user", "admin");
+      public static void main(String[] args) throws IOException {
+
+    String host = System.getProperty("target_host", "localhost");
+    int port = Integer.getInteger("target_port", 8080);
+    String testUrl = System.getProperty("test_url");
+
+    String baseUrl;
+
+    if (testUrl != null && !testUrl.isBlank()) {
+        // CI / environment-specific URL
+        baseUrl = testUrl;
+    } else {
+        // Local 
+        baseUrl = "http://" + host + ":" + port;
+    }
+  
+
+        String defaultUser = System.getProperty("perf_user", "admin");
         String defaultPass = System.getProperty("perf_pass", "admin123");
 
         testPlan(
