c80e43ddfae072c27f57bfca4b92c36f824ee2d9
HUANG Jun (Carlin) <huang.jun16@u.nus.edu>
2026-02-08 02:42:14 +0800

    Fix Unit Test to include AmazonS3 mock and adjust SpringBootTest properties

--- src/test/java/tech3/binitright/BinItRightApplicationTests.java
+++ src/test/java/tech3/binitright/BinItRightApplicationTests.java
@@ -1,5 +1,6 @@
 package tech3.binitright;
 
+import com.amazonaws.services.s3.AmazonS3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -7,12 +8,16 @@ import org.springframework.test.context.ActiveProfiles;
 import tech3.binitright.service.DigitalOceanStorageService;
 
 @SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
         properties = {
                 "openai.api.key=test-key"
         }
 )
 @ActiveProfiles("test")
 class BinItRightApplicationTests {
+
+    @MockBean
+    private AmazonS3 amazonS3;
 
     @MockBean
     private DigitalOceanStorageService digitalOceanStorageService;
