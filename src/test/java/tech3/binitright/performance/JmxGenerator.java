44dc0218b4c754050db304099a490e2869f1366f
e1616033 <150251429+lachupriya0993@users.noreply.github.com>
2026-02-07 09:50:39 +0800

    Update JmxGenerator.java

--- src/test/java/tech3/binitright/performance/JmxGenerator.java
+++ src/test/java/tech3/binitright/performance/JmxGenerator.java
@@ -3,14 +3,15 @@ package tech3.binitright.performance;
 import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
 import java.io.IOException;
 import java.time.Duration;
+import org.apache.http.entity.ContentType;
 
 public class JmxGenerator {
     public static void main(String[] args) throws IOException {
         String host = System.getProperty("target_host", "localhost");
         int port = Integer.getInteger("target_port", 8080);
         String baseUrl = "http://" + host + ":" + port;
-        String defaultUser = System.getProperty("perf_user"); 
-        String defaultPass = System.getProperty("perf_pass");
+        String defaultUser = System.getProperty("perf_user"."admin"); 
+        String defaultPass = System.getProperty("perf_pass","admin123");
 
         testPlan(
             threadGroup("Web_Admin_Load_Test")
@@ -25,7 +26,7 @@ public class JmxGenerator {
 
                     httpSampler("2_POST_Login_Submit", baseUrl + "/login")
                         .method("POST")
-                        .contentType("application/x-www-form-urlencoded")
+                        .contentType(ContentType.APPLICATION_FORM_URLENCODED)
                         .param("username", defaultUser)
                         .param("password", defaultPass)
                         .param("_csrf", "${csrf_token}"), 
