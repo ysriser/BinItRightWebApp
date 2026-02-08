2d1e6d785fd3d0fc456e76abec03acdcf53cd460
lakshmipriya pandian <150251429+lachupriya0993@users.noreply.github.com>
2026-02-08 12:29:08 +0800

    updated jmx

--- src/test/java/tech3/binitright/performance/JmxGenerator.java
+++ src/test/java/tech3/binitright/performance/JmxGenerator.java
@@ -53,7 +53,6 @@ public class JmxGenerator {
                         .holdIterating(20)
                         .children(
 
-                                /* ---- LOGIN ---- */
                                 httpSampler("API_Login", baseUrl + "/api/auth/login")
                                         .method("POST")
                                         .contentType(ContentType.APPLICATION_JSON)
@@ -64,38 +63,27 @@ public class JmxGenerator {
                                         .children(
                                                 jsonExtractor("extracted_token", "token")
                                         ),
-                                // STEP 2: PROTECTED RESOURCE (The test)
-                                httpSampler("Access Summary Profile API", baseUrl+ "/api/summary/profile")
+
+                                httpSampler("Access Summary Profile API", baseUrl + "/api/summary/profile")
                                         .header("Authorization", "Bearer ${extracted_token}"),
-                                httpSampler("Access User Accessories API", baseUrl+ "/api/user-accessories/my-items")
+
+                                httpSampler("Access User Accessories API", baseUrl + "/api/user-accessories/my-items")
                                         .header("Authorization", "Bearer ${extracted_token}"),
-                                httpSampler("Access News API", baseUrl+ "/api/news")
+
+                                httpSampler("Access News API", baseUrl + "/api/news")
                                         .header("Authorization", "Bearer ${extracted_token}"),
-                                httpSampler("Access Event API", baseUrl+ "/api/events")
+
+                                httpSampler("Access Event API", baseUrl + "/api/events")
                                         .header("Authorization", "Bearer ${extracted_token}"),
-                                httpSampler("Access Recycle history API", baseUrl+ "/api/recycle-history")
-                                        .header("Authorization", "Bearer ${extracted_token}"),
-                                httpSampler("API_POST_CheckIn", baseUrl + "/api/checkin")
-                                        .method("POST")
-                                        .contentType(ContentType.APPLICATION_JSON)
-                                        .body("{"
-                                                + "\"binId\": \"BIN-${__UUID}\"," 
-                                                + "\"wasteCategoryId\": 1,"          
-                                                + "\"fileName\": \"load_test_${__UUID}.jpg\"," 
-                                                + "\"quantity\": 2,"
-                                                + "\"rewardPoints\": 20,"
-                                                + "\"duration\": 15"
-                                                + "}")
-                                       .children(
-                                               httpHeaders()
-                                                  .header("Authorization", "Bearer ${extracted_token}")
-                                                        )
-                              
+
+                                httpSampler("Access Recycle History API", baseUrl + "/api/recycle-history")
+                                        .header("Authorization", "Bearer ${extracted_token}")
                         )
-              
+
         ).saveAsJmx("tests/load_test.jmx");
-
     }
-
-          
 }
+
+
+
+
