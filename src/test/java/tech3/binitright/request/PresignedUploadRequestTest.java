a719a3566f1e9dfe02b68fae9e513f02f5a90c21
sujithanandhini <sujithaarputhanantha@gmail.com>
2026-02-11 23:00:50 +0800

    unit test

---
+++ src/test/java/tech3/binitright/request/PresignedUploadRequestTest.java
@@ -0,0 +1,21 @@
+package tech3.binitright.request;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class PresignedUploadRequestTest {
+    @Test
+    void defaultConstructor_andSetter_shouldWork() {
+        PresignedUploadRequest request = new PresignedUploadRequest();
+
+        request.setUserId(100L);
+
+        assertEquals(100L, request.getUserId());
+    }
+
+    @Test
+    void parameterizedConstructor_shouldSetUserId() {
+        PresignedUploadRequest request = new PresignedUploadRequest(200L);
+
+        assertEquals(200L, request.getUserId());
+    }
+}
